03cb2168ed0c801584a831c613364145709cf72d
Suhas <84683870+BlueMoon-Suhas@users.noreply.github.com>
2026-02-20 14:16:03 -0500

    Delete SQL script 3.sql

--- SQL script 3.sql
+++
@@ -1,7 +0,0 @@
-select * from gold.customers
-
--- create external tables 
--- 3 steps to create a external table 
--- 1) create credentials 
--- 2) external data source
--- 3) external file format 
