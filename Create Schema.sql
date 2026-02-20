f538085364e91072794566c41820e62677641aa9
Suhas <84683870+BlueMoon-Suhas@users.noreply.github.com>
2026-02-20 14:15:44 -0500

    Delete Create Schema.sql

--- Create Schema.sql
+++
@@ -1,6 +0,0 @@
--- ##select * from OPENROWSET( BULK 'https://awprojectstorageaccount.dfs.core.windows.net/silver/AdventureWorks_Calendar/',
--- format = 'parquet'
-
--- ) as query1##
-
-CREATE SCHEMA gold;
